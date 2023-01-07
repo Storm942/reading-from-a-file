fa99813d8eaf96d54334a37435b408ab160036e6
Storm942 <91721279+Storm942@users.noreply.github.com>
2023-01-08 03:10:18 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -21,3 +21,17 @@ int main()
     fclose(fptr);
     return 0;
 }
+
+//ALTERNATIVE CODE
+
+#include<stdio.h>
+int main()
+{
+    FILE *fptr;
+    fptr=fname("hello.txt","r");
+
+    char ch;
+    fscanf(fptr,"%c",&ch);
+    printf("Character=",ch);
+    return 0;
+}
